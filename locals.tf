9ff6a7fac0d298178956ab388b4741c9e44b0a5d
TheodorWulff <104894109+TheodorWulff@users.noreply.github.com>
2022-09-01 16:50:23 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  snet_address_prefixes = setsubstract(flatten([ for snet in var.remote_vnet.subnets:  snet.address_prefixes]), var.remote_vnet.address_prefixes)
+  snet_address_prefixes = setsubtract(flatten([ for snet in var.remote_vnet.subnets:  snet.address_prefixes]), var.remote_vnet.address_prefixes)
 }
